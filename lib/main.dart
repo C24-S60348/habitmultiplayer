c602ec593ec9d054d7f26882f05dda2b0d30067a
Afwan Haziq <71675063+C24-S60348@users.noreply.github.com>
2025-04-09 00:13:27 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html_iframe/flutter_html_iframe.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'dart:html' as html;
+import 'package:web/web.dart' as web;
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
@@ -14,6 +26,8 @@ class MyApp extends StatelessWidget {
 
 //splash screen
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -144,26 +158,69 @@ class InsidePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Center(
-        child: Text(
-          'Open this link in your browser:\n$link',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 18),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          if (await canLaunch(link)) {
-            await launch(link);
-          } else {
-            ScaffoldMessenger.of(
-              context,
-            ).showSnackBar(SnackBar(content: Text('Could not launch $link')));
-          }
-        },
-        child: Icon(Icons.open_in_browser),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(title: Text(title), floating: true, snap: true),
+          SliverFillRemaining(child: WebIframeView(url: link)),
+        ],
       ),
     );
+  }
+}
+
+class WebIframeView extends StatelessWidget {
+  final String url;
+
+  iframeMatcher() {
+    return (context, element) => element.localName == 'iframe';
+  }
+
+  const WebIframeView({Key? key, required this.url}) : super(key: key);
+
+  @override
+  // Widget build(BuildContext context) {
+  //   return Html(
+  //     data: '''
+  //       <iframe
+  //         src="$url"
+  //         width="100%"
+  //         height="100%"
+  //         style="border:none;">
+  //       </iframe>
+  //     ''',
+  //     style: {
+  //       "iframe": Style(
+  //         width: Width(double.infinity),
+  //         height: Height(double.infinity),
+  //       ),
+  //     },
+  //   );
+  // }
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb) {
+      final String viewId = 'iframe-${url.hashCode}';
+
+      // Register once (safe for hot reload)
+      // ignore: undefined_prefixed_name
+      ui.platformViewRegistry.registerViewFactory(viewId, (int viewId) {
+        final iframe =
+            html.IFrameElement()
+              ..src = url
+              ..style.border = 'none'
+              ..style.width = '100%'
+              ..style.height = '100%';
+        return iframe;
+      });
+
+      return HtmlElementView(viewType: viewId);
+    } else {
+      final controller =
+          WebViewController()
+            ..setJavaScriptMode(JavaScriptMode.unrestricted)
+            ..loadRequest(Uri.parse(url));
+
+      return WebViewWidget(controller: controller);
+    }
   }
 }
