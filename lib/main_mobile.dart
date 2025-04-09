fb8d314cc0839efda901046bc794a3e926f7e5c2
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-04-09 14:10:51 +0800

    Create main_mobile.dart

---
+++ lib/main_mobile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:web/web.dart' as web;
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
