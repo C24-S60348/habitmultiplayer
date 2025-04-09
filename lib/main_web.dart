6cded7359f623591f1cd3f96c8086bf42bc73730
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-04-09 15:55:18 +0800

    Update main_web.dart

--- lib/main_web.dart
+++ lib/main_web.dart
@@ -10,6 +10,7 @@ import 'dart:html' as html;
 import 'package:web/web.dart' as web;
 import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'main.dart';
 
 void runAppEntry() {
   runApp(MyApp());
