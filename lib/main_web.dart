c8a4cef27ebb3e7dbb3d2aeb84ff42a5b55bda2a
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-04-09 14:10:20 +0800

    Create main_web.dart

---
+++ lib/main_web.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_html_iframe/flutter_html_iframe.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'dart:html' as html;
+import 'package:web/web.dart' as web;
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
