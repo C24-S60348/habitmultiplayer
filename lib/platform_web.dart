01493b0becc23022c333a79c658f915702e5fc92
C24-S60348 <71675063+C24-S60348@users.noreply.github.com>
2025-04-09 19:07:03 +0800

    Create platform_web.dart

---
+++ lib/platform_web.dart
@@ -0,0 +1,15 @@
+import 'dart:ui' as ui;
+import 'dart:html' as html;
+
+void registerIframe(String viewId, String url) {
+  // Register the iframe for web
+  // ignore: undefined_prefixed_name
+  ui.platformViewRegistry.registerViewFactory(viewId, (int viewId) {
+    final iframe = html.IFrameElement()
+      ..src = url
+      ..style.border = 'none'
+      ..style.width = '100%'
+      ..style.height = '100%';
+    return iframe;
+  });
+}
